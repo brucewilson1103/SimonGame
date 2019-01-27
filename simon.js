ab83585558a6ba791d5bd0f1b7122277966a5a03
bruceAlmighty <brucewilson1103@gmail.com>
2019-01-26 20:03:59 -0500

    added classes to buttons used settimeout to make the animation effect on the buttons

--- simon.js
+++ simon.js
@@ -1,6 +1,16 @@
-gamePattern = [];
+var gamePattern = [];
 
-buttonColors = ["red", "blue", "green", "yellow"];
+var buttonColors = ["red", "blue", "green", "yellow"];
+
+var userClickedPattern = [];
+
+$(".btn").click(function () {
+
+    var userChosenColor = $(this).attr("id");
+    userClickedPattern.push(userChosenColor);
+    //    console.log(userClickedPattern);
+    playsound(randomColorSelector);
+});
 
 function nextSequence() {
     var randomNumber = Math.floor(Math.random() * 4);
@@ -8,4 +18,22 @@ function nextSequence() {
 
     var randomColorSelector = buttonColors[randomNumber];
     gamePattern.push(randomColorSelector);
+
+    $("#" + randomColorSelector.fadeIn(100).fadeOut(100).fadeIn(100));
+
+   playsound(randomColorSelector);
+}
+
+function playsound (name){
+ var audio = new Audio("sounds/" + name + ".mp3");
+ audio.play();
+};
+
+function animateClick(currentColor) {
+    $("#" + currentColor).addclass("pressed");
+
+    setTimeout(function() {
+        $("#" + currentColor).removeClass("pressed");
+    }, 100);
+    
 }
