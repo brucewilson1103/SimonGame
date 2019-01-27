15e85629df4ddec5d65f24573caa41a09f6b18c9
bruceAlmighty <brucewilson1103@gmail.com>
2019-01-26 20:15:20 -0500

    level start and starting parameters

--- simon.js
+++ simon.js
@@ -4,17 +4,34 @@ var buttonColors = ["red", "blue", "green", "yellow"];
 
 var userClickedPattern = [];
 
+var started = false;
+var level = 0;
+
+$(document).keypress(function(){
+    if(!started){
+        $("#level-title").text("level "+ level);
+        nextSequence();
+        started = true;
+    }
+}
+);
+
 $(".btn").click(function () {
 
     var userChosenColor = $(this).attr("id");
     userClickedPattern.push(userChosenColor);
     //    console.log(userClickedPattern);
-    playsound(randomColorSelector);
+    playsound(userChosenColor);
+    animateClick(userChosenColor)
 });
 
 function nextSequence() {
+    level++;
+
+    $("#level-title").text("Level " + level);
+
     var randomNumber = Math.floor(Math.random() * 4);
-    console.log(randomNumber);
+    // console.log(randomNumber);
 
     var randomColorSelector = buttonColors[randomNumber];
     gamePattern.push(randomColorSelector);
